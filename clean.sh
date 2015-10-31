47bd539f6b785fe05eac576e91891c11ddcc169f
Kiarash Korki <kiarash96@users.sf.net>
2015-10-31 11:07:08 +0330

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-find codeforces/ -perm /111 -type f -exec rm "{}" \;
+find . ! -name clean.sh -perm /111 -type f -exec rm "{}" \;
+find . -name *.in -type f -exec rm "{}" \;
+find . -name *.out -type f -exec rm "{}" \;
 
