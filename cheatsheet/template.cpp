3ff978ff937f3dafe6a6f9ac95b99d2fb1943b28
Kiarash Korki <kiarash96@users.sf.net>
2015-11-27 17:45:50 +0330

    Added template.cpp

---
+++ cheatsheet/template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <locale>
+using namespace std;
+
+#define pb push_back
+
+typedef pair<int, int> pii;
+
+int main() {
+	ios::sync_with_stdio(0);
+
+
+
+	return 0;
+}
+
